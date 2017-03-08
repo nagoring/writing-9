474f8fd74956c8c47cf37ad51f8278f8dc5fdb8c
nagomi <nagoling@gmail.com>
2017-03-08 15:35:39 +0000

    test source formatting

--- tests/Any/Model/test-Calculator.php
+++ tests/Any/Model/test-Calculator.php
@@ -7,16 +7,13 @@ class Any_Model_CalculatorTest extends WP_UnitTestCase {
 	 * A single example test.
 	 */
 	function test_calcUnitPriceByOrder_zero_pattern() {
-			$calculator = Any_Model_Calculator::getInstance();
-			$order = new \stdClass();
-			$order->use_pro_writer = 0;
-			$order->visual_check = 0;
-			$order->title_creation = 0;
-			$order->format_setting = 0;
-			$this->assertSame(1, $calculator->_calcUnitPriceByOrder($order));
-		// any_test_method
-		// Closure::bind(function(){
-		// }, $this, 'Any_Model_Calculator')->__invoke();
+		$calculator = Any_Model_Calculator::getInstance();
+		$order = new \stdClass();
+		$order->use_pro_writer = 0;
+		$order->visual_check = 0;
+		$order->title_creation = 0;
+		$order->format_setting = 0;
+		$this->assertSame(1, $calculator->_calcUnitPriceByOrder($order));
 	}
 	function test_calcUnitPriceByOrder_total_pattern() {
 		$calculator = Any_Model_Calculator::getInstance();
