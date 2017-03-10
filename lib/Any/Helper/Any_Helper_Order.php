1e51aa21dc57217d92a46e3feaa9faf5b1372c22
nagomi <nagoling@gmail.com>
2017-03-10 15:55:32 +0000

    Add methods

--- lib/Any/Helper/Any_Helper_Order.php
+++ lib/Any/Helper/Any_Helper_Order.php
@@ -25,17 +25,87 @@ class Any_Helper_Order extends Any_Helper_Helper{
         return (int)$this->order->word_count;
     }
     public function post_date(){
-        return $this->order->post_date;
+        return esc_html($this->order->post_date);
     }
-    public function status(){
+    public function statusText(){
         return Any_Definition_EStatus::text($this->order->status);
     }
+    public function submitText(){
+        return Any_Definition_EStatus::submitText($this->order->status);
+    }
+    
     public function submitTag($order_id){
-        $submit_text = Any_Definition_EStatus::submitText($this->order->status);
+        $submit_text = $this->submitText();
         $url = get_admin_url() ."admin.php?page=writing9_order&order_ids[]={$order_id}";
-        
         $html = "<input name=\"save\" type=\"button\" class=\"button button-primary button-large\" id=\"publish\" value=\"{$submit_text}\" onclick=\"location.href='$url'\">";
         return $html;
+    }
+    public function title(){
+        return esc_html($this->order->title);
+    }
+    public function text_type(){
+        return esc_html($this->order->text_type);
+    }
+    public function endOfSentenceText(){
+        $array = Any_Definition_Form::getInstance()->get('end_of_sentence');
+        return any_safe($array, $this->order->end_of_sentence, '指定なし');
+    }
+    public function textTasteText(){
+        $array = Any_Definition_Form::getInstance()->get('text_taste');
+        return any_safe($array, $this->order->text_taste, '固め');
+    }
+    public function genreText(){
+        $array = Any_Definition_Form::getInstance()->get('genre');
+        return any_safe($array, $this->order->genre, '健康');
+    }
+    public function reference_url(){
+        return esc_html($this->order->reference_url);
+    }
+    public function main_word(){
+        return esc_html($this->order->main_word);
+    }
+    public function keyword1(){
+        return esc_html($this->order->keyword1);
+    }
+    public function keyword2(){
+        return esc_html($this->order->keyword2);
+    }
+    public function keyword3(){
+        return esc_html($this->order->keyword3);
+    }
+    public function keyword4(){
+        return esc_html($this->order->keyword4);
+    }
+    public function keyword5(){
+        return esc_html($this->order->keyword5);
+    }
+    public function ngword1(){
+        return esc_html($this->order->ngword1);
+    }
+    public function ngword2(){
+        return esc_html($this->order->ngword2);
+    }
+    public function visualCheckText(){
+        $array = Any_Definition_Form::getInstance()->get('visual_check');
+        return any_safe($array, $this->order->visual_check, '無');
+    }
+    public function useProWriterText(){
+        $array = Any_Definition_Form::getInstance()->get('use_pro_writer');
+        return any_safe($array, $this->order->use_pro_writer, '無');
+    }
+    public function note(){
+        return esc_html($this->order->note);
+    }
+    public function titleCreationText(){
+        $array = Any_Definition_Form::getInstance()->get('title_creation');
+        return any_safe($array, $this->order->title_creation, '無');
+    }
+    public function formatSettingText(){
+        $array = Any_Definition_Form::getInstance()->get('format_setting');
+        return any_safe($array, $this->order->format_setting, '無');
+    }
+    public function format_setting_note(){
+        return esc_html($this->order->format_setting_note);
     }
 }
 
