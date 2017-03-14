36a338b3a4e5e262bb3c9de0a29528ca2764ab8a
nagomi <nagoling@gmail.com>
2017-03-14 14:19:19 +0000

    Modified  Add check_for_authority

--- lib/writing9_common.php
+++ lib/writing9_common.php
@@ -94,5 +94,5 @@ function any_writing9_set_setting_and_get(){
 	return $option;
 }
 function any_writing9_check_for_authority(){
-	return current_user_can('administrator')
+	return current_user_can('administrator');
 }
