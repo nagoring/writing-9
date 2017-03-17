1dac6f0d4320eab00bf52920c98882cd03b56f0a
nagomi <nagoling@gmail.com>
2017-03-17 11:25:11 +0000

    update mail

---
+++ tests/Any/Model/test-Any_Model_Mailer.php
@@ -0,0 +1,105 @@
+<?php
+class Any_Model_MailerTest extends WP_UnitTestCase {
+	function setUp(){
+		parent::setUp();
+	}
+	/**
+	 * A single example test.
+	 */
+	function test__createMailBodyParameter_pt1() {
+		$mailer = Any_Model_Mailer::getInstance();
+		$order = new stdClass();
+		$order->title = 'タイトルです';
+		$order->text_type = '文章タイプ・用途です';
+		$order->end_of_sentence = '1';
+		$order->text_taste = '2';
+		$order->genre = '34';
+		$order->reference_url = 'http://example.com';
+		$order->main_word = 'メインのワードです';
+		$order->keyword1 = 'キーワード1です';
+		$order->keyword2 = 'キーワード2です';
+		$order->keyword3 = 'キーワード3です';
+		$order->keyword4 = 'キーワード4です';
+		$order->keyword5 = 'キーワード5です';
+		$order->ngword1 = 'NGワード1です';
+		$order->ngword2 = 'NGワード2です';
+		$order->number_articles = '4';
+		$order->word_count = '1200';
+		$order->visual_check = '1';
+		$order->use_pro_writer = '1';
+		$order->note  = '特記事項です';
+		$order->title_creation  = '1';
+		$order->format_setting  = '1';
+		$order->format_setting_note  = 'ご要望です。';
+		
+		$body = $mailer->_createMailBodyParameter($order);
+		$tests_path = ANY_WRITING9_TEST_PATH . '/input/Any_Helper_Order/_createMailBodyParameter_pt1.txt';
+		$contents = file_get_contents($tests_path);
+		
+		$this->assertSame($contents, $body);
+		// $this->assertSame(preg_replace('/\s/', '', $contents), preg_replace('/\s/', '', $body));
+	}
+	function test__createMailBodyParameter_pt2() {
+		$mailer = Any_Model_Mailer::getInstance();
+		$order = new stdClass();
+		$order->title = 'タイトルです';
+		$order->text_type = '文章タイプ・用途です';
+		$order->end_of_sentence = '0';
+		$order->text_taste = '1';
+		$order->genre = '1';
+		$order->reference_url = 'http://example.com';
+		$order->main_word = 'メインのワードです';
+		$order->keyword1 = 'キーワード1です';
+		$order->keyword2 = 'キーワード2です';
+		$order->keyword3 = 'キーワード3です';
+		$order->keyword4 = 'キーワード4です';
+		$order->keyword5 = 'キーワード5です';
+		$order->ngword1 = 'NGワード1です';
+		$order->ngword2 = 'NGワード2です';
+		$order->number_articles = '4';
+		$order->word_count = '1200';
+		$order->visual_check = '0';
+		$order->use_pro_writer = '0';
+		$order->note  = '特記事項です';
+		$order->title_creation  = '0';
+		$order->format_setting  = '0';
+		$order->format_setting_note  = 'ご要望です。';
+		
+		$body = $mailer->_createMailBodyParameter($order);
+		$tests_path = ANY_WRITING9_TEST_PATH . '/input/Any_Helper_Order/_createMailBodyParameter_pt2.txt';
+		$contents = file_get_contents($tests_path);
+		
+		$this->assertSame($contents, $body);
+	}
+	function test__createMailBodyParameter_pt2_remove_empty() {
+		$mailer = Any_Model_Mailer::getInstance();
+		$order = new stdClass();
+		$order->title = 'タイトルです';
+		$order->text_type = '文章タイプ・用途です';
+		$order->end_of_sentence = '0';
+		$order->text_taste = '1';
+		$order->genre = '1';
+		$order->reference_url = 'http://example.com';
+		$order->main_word = 'メインのワードです';
+		$order->keyword1 = 'キーワード1です';
+		$order->keyword2 = 'キーワード2です';
+		$order->keyword3 = 'キーワード3です';
+		$order->keyword4 = 'キーワード4です';
+		$order->keyword5 = 'キーワード5です';
+		$order->ngword1 = 'NGワード1です';
+		$order->ngword2 = 'NGワード2です';
+		$order->number_articles = '4';
+		$order->word_count = '1200';
+		$order->visual_check = '0';
+		$order->use_pro_writer = '0';
+		$order->note  = '特記事項です';
+		$order->title_creation  = '0';
+		$order->format_setting  = '0';
+		$order->format_setting_note  = 'ご要望です。';
+		
+		$body = $mailer->_createMailBodyParameter($order);
+		$tests_path = ANY_WRITING9_TEST_PATH . '/input/Any_Helper_Order/_createMailBodyParameter_pt2.txt';
+		$contents = file_get_contents($tests_path);
+		$this->assertSame(preg_replace('/\s/', '', $contents), preg_replace('/\s/', '', $body));
+	}
+}
