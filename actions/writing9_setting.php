6198f4b741c997121a7b45d11ecf83df4fd006b4
nagomi <nagomi.github@gmail.com>
2017-04-03 20:42:48 +0900

    Set disable ipn and private_key changely

--- actions/writing9_setting.php
+++ actions/writing9_setting.php
@@ -11,13 +11,10 @@ function writing9_setting() {
 	
     if(isset($_POST['original_publish']) && strlen($_POST['original_publish']) > 0 && check_admin_referer('writing9_setting', '_writing9_nonce')){
 		$response->set('email', any_safe($_POST, 'email', ''));
-		$response->set('ipn', any_safe($_POST, 'ipn', ''));
-		$response->set('private_key', any_safe($_POST, 'private_key', ''));
 		if(_writing9_validate_setting()){
 			$save = array();
+			$save = any_writing9_set_setting_and_get();
 			$save['email'] = $response->get('email');
-			$save['ipn'] = $response->get('ipn');
-			$save['private_key'] = $response->get('private_key');
 			$save['api_key'] = any_writing9_api_key();
 			update_option('Any_Writing9', $save, 'no');
 			$option = get_option('Any_Writing9', false);
@@ -26,9 +23,6 @@ function writing9_setting() {
     }
 	$option = any_writing9_set_setting_and_get();
 	$response->set('email', $option['email']);
-	$response->set('ipn', $option['ipn']);
-	$response->set('private_key', $option['private_key']);
-	// $response->set('order_id', null);
     $view->render('views/v_writing9_setting.php', array(
 		'form' => $form,
 		'error' => $error,
@@ -39,9 +33,6 @@ function _writing9_validate_setting(){
 	$error = Any_Core_Error::getInstance();
     if(!isset($_POST['email']) || empty($_POST['email'])){
 		$error->add("メールアドレスを入力してください");
-    }
-    if(!isset($_POST['ipn']) || empty($_POST['ipn'])){
-    	$error->add("PayPalのIPN受信チェックのためのパラメータを入力してください");
     }
     return $error->isNotError();
 }
